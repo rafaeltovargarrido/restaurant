4c0019963e735f9c71fb31f7a8d01f193638556f
Rafael Tovar <rafaeltovar1992@gmail.com>
2020-05-05 22:43:05 +0200

    r

--- gen_artefact.sh
+++ gen_artefact.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+TAGFILE=artefact/tags
+ls | grep *source* | awk -F'-' '{print $NF}' >> $TAGFILE
+cat *source*/.git/short_ref >> $TAGFILE
+cat *source*/.git/ref >> $TAGFILE
+
+cp -rf *source*/* artefact/
+cp -rf *source*/.env artefact/
+
 cp -rf artefacts/* artefact/
 
 ls artefact/
